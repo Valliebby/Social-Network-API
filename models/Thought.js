99ceeb324d07740bce140e8a7cb1b29edc8592e9
Valerie Elizabeth <119483866+Valliebby@users.noreply.github.com>
2023-07-21 12:03:54 -0500

    Node Mod

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,6 @@
 const { Schema, model } = require('mongoose');
 const reactionSchema = require('./Reaction');
-const dateFormat = require('../utils/dateFormat');
+
 
 const thoughtSchema = new Schema(
   {
@@ -13,7 +13,7 @@ const thoughtSchema = new Schema(
     createdAt: {
       type: Date,
       default: Date.now,
-      get: timestamp => dateFormat(timestamp)
+      get: timestamp =>timestamp.toLocaleDateString()
     },
     username: {
       type: String,
